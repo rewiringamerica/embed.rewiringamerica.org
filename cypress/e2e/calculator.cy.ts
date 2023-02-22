a209a452209f89be2be8eb3cd032613e87ba82b7
Tom Carden <tomc@rewiringamerica.org>
2023-02-22 10:20:14 -0700

    add cypress test and gh action

---
+++ cypress/e2e/calculator.cy.ts
@@ -0,0 +1,25 @@
+describe('template spec', () => {
+  it('passes', () => {
+    cy.visit('http://localhost:1234');
+    cy.get('rewiring-america-calculator').should('exist');
+    // FIXME: is there a way to do this less repetitively?
+    // Doing const shadow = cy.get(...).shadow() didn't seem to work.
+    cy.get('rewiring-america-calculator').shadow()
+      .contains('Upfront Discounts')
+      .siblings()
+      .contains('$14,000');
+    cy.get('rewiring-america-calculator')
+      .shadow()
+      .contains('Available Tax Credits')
+      .siblings()
+      .contains('$9,850');
+    cy.get('rewiring-america-calculator')
+      .shadow()
+      .contains('Estimated Bill Savings Per Year')
+      .siblings()
+      .contains('$1,050');
+    cy.get('rewiring-america-calculator')
+      .shadow()
+      .contains('Covers up to 50% of costs');
+  })
+})
