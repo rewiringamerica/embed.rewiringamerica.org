bda9587e9d5c15624757ac13b068e13baec6dd4e
Tom Carden <tomc@rewiringamerica.org>
2023-02-22 14:12:41 -0700

    update API key for working copy; fix error handling

--- src/calculator.ts
+++ src/calculator.ts
@@ -103,7 +103,23 @@ export class RewiringAmericaCalculator extends LitElement {
         });
         if (response.status >= 400) {
           console.error(response);
-          throw new Error(response.statusText);
+          // statusText isn't always set, but it's a reasonable proxy for a human readable error if it is:
+          let message = response.statusText;
+          try {
+            const error = await response.json();
+            console.error(error);
+            if (error.title && error.detail) {
+              // Zuplo's API key errors have this form:
+              message = `${error.title}: ${error.detail}`;
+            } else if (error.message && error.error) {
+              // Rewiring America's API errors have this form:
+              message = `${error.error}: ${error.message}`;
+            }
+          } catch (e) {
+            // if we couldn't get anything off the response, just go with something generic:
+            message = 'Error loading incentives.'
+          }
+          throw new Error(message);
         }
         return response.json();
       },
