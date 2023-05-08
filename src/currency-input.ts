6266f1fdf002ea2a5738f1ac4bdac12a0c9410f2
Tom Carden <tomc@rewiringamerica.org>
2023-05-08 10:03:25 -0600

    WIP

--- src/currency-input.ts
+++ src/currency-input.ts
@@ -1,7 +1,8 @@
-import { LitElement, css, html, PropertyValues } from 'lit';
+import { LitElement, html, PropertyValues } from 'lit';
 import { customElement, property } from 'lit/decorators.js';
 import { ref } from 'lit/directives/ref';
-import AutoNumeric from 'autonumeric';
+import IMask from 'imask/esm/imask'; // imports only factory
+import 'imask/esm/masked/number'; // add needed features
 import 'element-internals-polyfill';
 
 @customElement('ra-currency-input')
@@ -22,7 +23,7 @@ export class CurrencyInput extends LitElement {
   required = false;
 
   // this is optional because it can't exist until the input is rendered
-  private autonumeric?: AutoNumeric;
+  private mask?: IMask.InputMask<Opts>;
 
   // this is optional because we set it in connectedCallback()
   private internals?: ElementInternals;
@@ -38,7 +39,7 @@ export class CurrencyInput extends LitElement {
   }
 
   onChange() {
-    const num = this.autonumeric!.getNumericString();
+    const num = this.mask!.unmaskedValue;
     if (num !== null) {
       this.value = num;
       // this ensures that when <ra-currency-input> is inside a form, new FormData(form) will pick up our value
@@ -62,35 +63,45 @@ export class CurrencyInput extends LitElement {
   }
 
   override disconnectedCallback() {
-    this.autonumeric?.detach();
+    this.mask?.destroy();
     this.removeEventListener('keydown', this.onKeydown);
     super.disconnectedCallback();
   }
 
   inputRefChanged(input?: Element) {
     if (input === undefined) {
-      this.autonumeric?.detach();
+      this.mask?.destroy();
     } else {
-      this.autonumeric = new AutoNumeric(input as HTMLInputElement, this.value, AutoNumeric.getPredefinedOptions().NorthAmerican);
-      this.updateAutonumericOptions();
+      this.mask = IMask(input as HTMLInputElement, {
+        mask: '$num',
+        blocks: {
+          num: {
+            mask: Number,
+            scale: 0,  // digits after point, 0 for integers
+            signed: false,  // disallow negative
+            thousandsSeparator: ',',  // any single char
+            padFractionalZeros: false,  // if true, then pads zeros at end to the length of scale
+            normalizeZeros: true,  // appends or removes zeros at ends
+          }
+        }
+      });
+      this.updatemaskOptions();
     }
   }
 
   override willUpdate(changedProperties: PropertyValues<this>) {
     if (changedProperties.has('min') || changedProperties.has('max')) {
-      this.updateAutonumericOptions();
+      this.updatemaskOptions();
     }
-    if (changedProperties.has('value')) {
-      this.autonumeric?.set(this.value);
+    if (changedProperties.has('value') && this.mask) {
+      this.mask.unmaskedValue = this.value;
     }
   }
 
-  updateAutonumericOptions() {
-    this.autonumeric?.update({
-      minimumValue: this.min.toString(),
-      maximumValue: this.max.toString(),
-      decimalPlaces: 0,
-      upDownStep: 1000,
+  updatemaskOptions() {
+    this.mask?.updateOptions({
+      min: this.min,
+      max: this.max,
     });
   }
 
