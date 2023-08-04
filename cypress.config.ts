4d548695e95c1fad874e522593ea13f81a604ce6
Owen Yamauchi <owen@rewiringamerica.org>
2023-08-04 14:57:16 -0400

    Add Prettier (#10)
    I haven't examined every single change. It's mostly single-quotes,
    trailing commas, and indentation/linebreaking. A few places in
    calculator.ts where parentheses were added:
    
    ```typescript
    const c = something as string || '';
    // becomes
    const c = (something as string) || '';
    ```
    
    This doesn't seem quite right to me -- I think it should just be
    `something ?? ''`, or `(something ?? '') as OtherType` -- but I wanted
    to keep this PR mechanical.
    
    It turns out Prettier is also smart enough to prettify HTML and CSS
    within template literals, which is very cool.

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,7 +1,7 @@
-import { defineConfig } from "cypress";
+import { defineConfig } from 'cypress';
 
 export default defineConfig({
-  projectId: "4uypg2",
+  projectId: '4uypg2',
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
--- src/calculator-form.ts
+++ src/calculator-form.ts
@@ -6,51 +6,48 @@ import './currency-input';
 import '@shoelace-style/shoelace/dist/themes/light.css';
 import '@shoelace-style/shoelace/dist/components/tooltip/tooltip.js';
 
-
 const buttonStyles = css`
-button {
-  appearance: none;
-  font-family: inherit;
-  font-size: 16px;
-  line-height: 28px;
-  padding: 8px;
-  background-color: var(--ra-embed-primary-button-background-color);
-  border: 1px solid var(--ra-embed-primary-button-background-color);
-  border-radius: 4px;
-  font-weight: 600;
-  margin: 4px 0 0 0;
-  cursor: pointer;
-  color: var(--ra-embed-primary-button-text-color);
-  outline: 0;
-  width: 100%;
-}
+  button {
+    appearance: none;
+    font-family: inherit;
+    font-size: 16px;
+    line-height: 28px;
+    padding: 8px;
+    background-color: var(--ra-embed-primary-button-background-color);
+    border: 1px solid var(--ra-embed-primary-button-background-color);
+    border-radius: 4px;
+    font-weight: 600;
+    margin: 4px 0 0 0;
+    cursor: pointer;
+    color: var(--ra-embed-primary-button-text-color);
+    outline: 0;
+    width: 100%;
+  }
 
-button:hover {
-  background-color: var(--ra-embed-primary-button-background-hover-color);
-}
+  button:hover {
+    background-color: var(--ra-embed-primary-button-background-hover-color);
+  }
 
-/* shoelace style overrides */
-sl-tooltip {
-  --max-width: var(--ra-tooltip-max-width);
-  --sl-tooltip-arrow-size: var(--ra-tooltip-arrow-size);
-  --sl-tooltip-padding: var(--ra-tooltip-padding);
-  --sl-tooltip-background-color: var(--ra-tooltip-background-color);
-  --sl-tooltip-color: var(--ra-tooltip-color);
-  --sl-tooltip-border-radius: var(--ra-tooltip-border-radius);
-  --sl-tooltip-font-family: var(--ra-embed-font-family);
-  --sl-tooltip-font-size: var(--ra-embed-font-size);
-  --sl-tooltip-line-height: var(--ra-embed-line-height);
-  --sl-tooltip-font-weight: var(--ra-embed-font-weight);
-  --sl-z-index-tooltip: 10000;
-}
+  /* shoelace style overrides */
+  sl-tooltip {
+    --max-width: var(--ra-tooltip-max-width);
+    --sl-tooltip-arrow-size: var(--ra-tooltip-arrow-size);
+    --sl-tooltip-padding: var(--ra-tooltip-padding);
+    --sl-tooltip-background-color: var(--ra-tooltip-background-color);
+    --sl-tooltip-color: var(--ra-tooltip-color);
+    --sl-tooltip-border-radius: var(--ra-tooltip-border-radius);
+    --sl-tooltip-font-family: var(--ra-embed-font-family);
+    --sl-tooltip-font-size: var(--ra-embed-font-size);
+    --sl-tooltip-line-height: var(--ra-embed-line-height);
+    --sl-tooltip-font-weight: var(--ra-embed-font-weight);
+    --sl-z-index-tooltip: 10000;
+  }
 
-sl-tooltip::part(body) {
-  box-shadow: var(--ra-tooltip-box-shadow);
-  border-radius: var(--sl-tooltip-border-radius);
-  border: var(--ra-tooltip-border);
-}
-
-
+  sl-tooltip::part(body) {
+    box-shadow: var(--ra-tooltip-box-shadow);
+    border-radius: var(--sl-tooltip-border-radius);
+    border: var(--ra-tooltip-border);
+  }
 `;
 
 export const formStyles = [inputStyles, buttonStyles, selectStyles];
@@ -63,67 +60,127 @@ const OWNER_STATUS_OPTIONS: OptionParam[] = [
 const TAX_FILING_OPTIONS: OptionParam[] = [
   { value: 'single', label: 'Single' },
   { value: 'joint', label: 'Joint' },
-  { value: 'hoh', label: 'Head of Household' }
+  { value: 'hoh', label: 'Head of Household' },
 ];
 
-const HOUSEHOLD_SIZE_OPTIONS: OptionParam[] = [1, 2, 3, 4, 5, 6, 7, 8].map(count => {
-  return {
-    label: `${count} ${count == 1 ? 'person' : 'people'}`,
-    value: count.toString()
-  } as OptionParam
-});
+const HOUSEHOLD_SIZE_OPTIONS: OptionParam[] = [1, 2, 3, 4, 5, 6, 7, 8].map(
+  count => {
+    return {
+      label: `${count} ${count == 1 ? 'person' : 'people'}`,
+      value: count.toString(),
+    } as OptionParam;
+  },
+);
 
-export const formTemplate = ([zip,
-  ownerStatus,
-  householdIncome,
-  taxFiling,
-  householdSize]: Array<string>, onSubmit: Function) => html`
-<div class="card card-content">
-  <h1>How much money will you get with the Inflation Reduction Act?</h1>
-  <form @submit=${onSubmit}>
-    <div class="grid-3-2">
-      <div>
-        <label for="zip">
-          Zip <sl-tooltip content="Your zip code helps determine the amount of discounts and tax credits you qualify for." hoist>${questionIcon(18, 18)}</sl-tooltip><br>
-          <input tabindex="0" id="zip" placeholder="12345" name="zip" required type="text" value="${zip}" minlength="5" maxlength="5" inputmode="numeric" pattern="[0-9]{5}">
-        </label>
+export const formTemplate = (
+  [zip, ownerStatus, householdIncome, taxFiling, householdSize]: Array<string>,
+  onSubmit: Function,
+) => html`
+  <div class="card card-content">
+    <h1>How much money will you get with the Inflation Reduction Act?</h1>
+    <form @submit=${onSubmit}>
+      <div class="grid-3-2">
+        <div>
+          <label for="zip">
+            Zip
+            <sl-tooltip
+              content="Your zip code helps determine the amount of discounts and tax credits you qualify for."
+              hoist
+              >${questionIcon(18, 18)}</sl-tooltip
+            ><br />
+            <input
+              tabindex="0"
+              id="zip"
+              placeholder="12345"
+              name="zip"
+              required
+              type="text"
+              value="${zip}"
+              minlength="5"
+              maxlength="5"
+              inputmode="numeric"
+              pattern="[0-9]{5}"
+            />
+          </label>
+        </div>
+        <div>
+          <label for="owner_status">
+            Homeowners Status
+            <sl-tooltip
+              content="Homeowners and renters qualify for different incentives."
+              hoist
+              >${questionIcon(18, 18)}</sl-tooltip
+            ><br />
+            ${select({
+              id: 'owner_status',
+              required: true,
+              options: OWNER_STATUS_OPTIONS,
+              currentValue: ownerStatus,
+              tabIndex: 0,
+            })}
+          </label>
+        </div>
+        <div>
+          <label for="household_income">
+            Household Income
+            <sl-tooltip
+              content="Enter your gross income (income before taxes). Include wages and salary plus other forms of income, including pensions, interest, dividends, and rental income. If you are married and file jointly, include your spouse's income"
+              hoist
+              >${questionIcon(18, 18)}</sl-tooltip
+            ><br />
+            <ra-currency-input
+              id="household_income"
+              placeholder="$60,000"
+              name="household_income"
+              required
+              value=${householdIncome}
+              min="0"
+              max="100000000"
+            ></ra-currency-input>
+          </label>
+        </div>
+        <div>
+          <label for="tax_filing">
+            Tax Filing
+            <sl-tooltip hoist
+              ><div slot="content">
+                Select "Head of Household" if you have a child or relative
+                living with you, and you pay more than half the costs of your
+                home. Select "Joint" if you file your taxes as a married
+                couple."
+              </div>
+              ${questionIcon(18, 18)}</sl-tooltip
+            ><br />
+            ${select({
+              id: 'tax_filing',
+              required: true,
+              options: TAX_FILING_OPTIONS,
+              currentValue: taxFiling,
+              tabIndex: 0,
+            })}
+          </label>
+        </div>
+        <div>
+          <label for="household_size">
+            Household Size
+            <sl-tooltip
+              content="Include anyone you live with who you claim as a dependent on your taxes, and your spouse or partner if you file taxes together."
+              hoist
+              >${questionIcon(18, 18)}</sl-tooltip
+            ><br />
+            ${select({
+              id: 'household_size',
+              required: true,
+              options: HOUSEHOLD_SIZE_OPTIONS,
+              currentValue: householdSize,
+              tabIndex: 0,
+            })}
+          </label>
+        </div>
+        <div>
+          <button type="submit">Calculate! ${downIcon(18, 18)}</button>
+        </div>
       </div>
-      <div>
-        <label for="owner_status">
-          Homeowners Status <sl-tooltip content="Homeowners and renters qualify for different incentives." hoist>${questionIcon(18, 18)}</sl-tooltip><br>
-          ${select({ id: 'owner_status', required: true, options: OWNER_STATUS_OPTIONS, currentValue: ownerStatus, tabIndex: 0 })}
-        </label>
-      </div>
-      <div>
-        <label for="household_income">
-          Household Income <sl-tooltip content="Enter your gross income (income before taxes). Include wages and salary plus other forms of income, including pensions, interest, dividends, and rental income. If you are married and file jointly, include your spouse\'s income" hoist>${questionIcon(18, 18)}</sl-tooltip><br>
-          <ra-currency-input
-            id="household_income"
-            placeholder="$60,000"
-            name="household_income"
-            required
-            value=${householdIncome}
-            min="0"
-            max="100000000"
-          ></ra-currency-input>
-        </label>
-      </div>
-      <div>
-        <label for="tax_filing">
-          Tax Filing <sl-tooltip hoist><div slot="content">Select "Head of Household" if you have a child or relative living with you, and you pay more than half the costs of your home. Select "Joint" if you file your taxes as a married couple."</div>${questionIcon(18, 18)}</sl-tooltip><br>
-          ${select({ id: 'tax_filing', required: true, options: TAX_FILING_OPTIONS, currentValue: taxFiling, tabIndex: 0 })}
-        </label>
-      </div>
-      <div>
-        <label for="household_size">
-          Household Size <sl-tooltip content="Include anyone you live with who you claim as a dependent on your taxes, and your spouse or partner if you file taxes together." hoist>${questionIcon(18, 18)}</sl-tooltip><br>
-          ${select({ id: 'household_size', required: true, options: HOUSEHOLD_SIZE_OPTIONS, currentValue: householdSize, tabIndex: 0 })}
-        </label>
-      </div>
-      <div>
-        <button type="submit">Calculate! ${downIcon(18, 18)}</button>
-      </div>
-    </div>
-  </form>
-</div>
+    </form>
+  </div>
 `;
