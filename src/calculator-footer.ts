be7397d3d7b5793696b5bb84359c7871a46a7cbd
Owen Yamauchi <owen@rewiringamerica.org>
2023-08-04 19:14:22 -0400

    Factor out some common stuff from calculator.ts (#12)
    I want to copy this file for the RI calculator. To avoid too much
    duplication, this factors out some common logic: the "fetch from API
    and interpret errors" code, and the static footer.
    
    It also changes the `api-path` attribute of the calculator element to
    `api-host`, which should contain only the protocol and hostname, not
    the path. Since the RI calculator will be fetching multiple paths, I
    want to separate path from host. `api-path` is not publicly
    documented, so I think it's fine to remove it.

---
+++ src/calculator-footer.ts
@@ -0,0 +1,21 @@
+import { html } from 'lit';
+
+export const CALCULATOR_FOOTER = html`<div class="calculator__footer">
+  <p>
+    Calculator&nbsp;by&nbsp;
+    <a target="_blank" href="https://www.rewiringamerica.org">
+      Rewiring&nbsp;America
+    </a>
+    •
+    <a
+      target="_blank"
+      href="https://content.rewiringamerica.org/view/privacy-policy.pdf"
+    >
+      Privacy&nbsp;Policy
+    </a>
+    •
+    <a target="_blank" href="https://content.rewiringamerica.org/api/terms.pdf">
+      Terms
+    </a>
+  </p>
+</div>`;
