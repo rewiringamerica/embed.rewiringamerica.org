0fca502b9892594ec8ab2810e83a3f78cbf63514
Tom Carden <tomc@rewiringamerica.org>
2023-10-03 08:47:14 -0600

    tomc/input font (#34)
    - use GT America for inputs
    - use tabular numbers for numeric inputs
    
    Test plan: inspect the input and check that the font is set to GT
    America and not the system font.
    
    Closes https://app.asana.com/0/1205057814651000/1205636582326124/f

--- src/styles/input.ts
+++ src/styles/input.ts
@@ -2,6 +2,7 @@ import { css } from 'lit';
 
 export const inputStyles = css`
   input {
+    font-family: inherit;
     padding: var(--ra-input-padding);
     font-size: var(--ra-input-font-size);
     line-height: var(--ra-input-line-height);
@@ -14,5 +15,9 @@ export const inputStyles = css`
   input:focus {
     box-shadow: 0 0 0 2px var(--ra-input-focus-color) inset;
     outline: none;
+  }
+
+  input[inputmode='numeric'] {
+    font-variant-numeric: tabular-nums;
   }
 `;
