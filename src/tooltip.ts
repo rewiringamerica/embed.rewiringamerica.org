f8027a812d26568d99a0101ab0ae91fd18f0b348
Owen Yamauchi <owen@rewiringamerica.org>
2023-11-03 15:44:40 -0400

    Actually with the blur handler you don't even need the whole-document mousedown listener

--- src/tooltip.ts
+++ src/tooltip.ts
@@ -29,26 +29,6 @@ function hideTooltip(event: FocusEvent) {
 }
 
 /**
- * When a tooltip is shown, add a one-time mousedown listener to the document
- * that dismisses any open tooltips.
- */
-function onTooltipShow(showEvent: CustomEvent) {
-  // Calling abort() on this will remove the event listener
-  const abortController = new AbortController();
-  const tooltip = showEvent.target as SlTooltip;
-
-  document.addEventListener(
-    'mousedown',
-    mouseEvent => {
-      tooltip.hide();
-      abortController.abort();
-      mouseEvent.stopPropagation();
-    },
-    { signal: abortController.signal },
-  );
-}
-
-/**
  * A wrapper for sl-tooltip that renders a clickable question-mark icon to
  * trigger it, and makes it so that clicking anywhere other than the tooltip
  * dismisses it.
@@ -58,12 +38,7 @@ export function tooltipButton(
   iconSize: number,
 ): HTMLTemplateResult {
   return html`
-    <sl-tooltip
-      content=${text}
-      trigger="manual"
-      hoist
-      @sl-show=${onTooltipShow}
-    >
+    <sl-tooltip content=${text} trigger="manual" hoist>
       <button
         class="tooltip-icon"
         type="button"
