2bbb713e55160bb63d58870637554f3b798080f6
Owen Yamauchi <owen@rewiringamerica.org>
2023-11-27 17:18:54 -0500

    Type safety for SafeLocalStorage
    ## Description
    
    Inspired by HTMLElementTagNameMap, this lets you associate a type with each local storage key you use.
    
    ## Test Plan
    
    It typechecks

--- src/safe-local-storage.ts
+++ src/safe-local-storage.ts
@@ -1,9 +1,24 @@
 /**
+ * Augment this interface with a property whose name is the local storage key,
+ * and whose type is the type of the values you will store under that key.
+ *
+ * declare module './safe-local-storage' {  // or however you import this
+ *   interface SafeLocalStorageMap {
+ *     'my-key': MyType
+ *   }
+ * }
+ */
+export interface SafeLocalStorageMap {}
+
+/**
  * A wrapper around window.localStorage that silences errors and handles JSON
  * encoding/decoding.
  */
 class SafeLocalStorage {
-  setItem<T>(key: string, value: T) {
+  setItem<T extends keyof SafeLocalStorageMap>(
+    key: T,
+    value: SafeLocalStorageMap[T],
+  ) {
     try {
       localStorage.setItem(key, JSON.stringify(value));
     } catch (_) {
@@ -11,7 +26,9 @@ class SafeLocalStorage {
     }
   }
 
-  getItem<T>(key: string): T | null {
+  getItem<T extends keyof SafeLocalStorageMap>(
+    key: T,
+  ): SafeLocalStorageMap[T] | null {
     try {
       const value = localStorage.getItem(key);
       return value ? JSON.parse(value) : null;
@@ -20,7 +37,7 @@ class SafeLocalStorage {
     }
   }
 
-  removeItem(key: string) {
+  removeItem(key: keyof SafeLocalStorageMap) {
     try {
       localStorage.removeItem(key);
     } catch (_) {
