d3ca5f7d2d892e8c7bb801b4fa3ae13305b0c883
Tom Carden <tomc@rewiringamerica.org>
2023-11-28 16:59:03 -0700

    allow overriding footer with a slot

--- src/calculator-footer.ts
+++ src/calculator-footer.ts
@@ -3,21 +3,27 @@ import { msg } from '@lit/localize';
 
 const toNbsp = (s: string) => s.replace(' ', '\u00a0');
 
-export const CALCULATOR_FOOTER = () => html`<div class="calculator__footer">
-  <p>
-    ${toNbsp(msg('Calculator by', { desc: 'followed by "Rewiring America"' }))}
-    <a target="_blank" href="https://www.rewiringamerica.org"
-      >Rewiring&nbsp;America</a
-    >
-    •
-    <a
-      target="_blank"
-      href="https://content.rewiringamerica.org/view/privacy-policy.pdf"
-      >${toNbsp(msg('Privacy Policy'))}</a
-    >
-    •
-    <a target="_blank" href="https://content.rewiringamerica.org/api/terms.pdf"
-      >${toNbsp(msg('Terms', { desc: 'as in terms of service' }))}</a
-    >
-  </p>
-</div>`;
+export const CALCULATOR_FOOTER = () => html`<slot name="footer">
+  <div class="calculator__footer">
+    <p>
+      ${toNbsp(
+        msg('Calculator by', { desc: 'followed by "Rewiring America"' }),
+      )}
+      <a target="_blank" href="https://www.rewiringamerica.org"
+        >Rewiring&nbsp;America</a
+      >
+      •
+      <a
+        target="_blank"
+        href="https://content.rewiringamerica.org/view/privacy-policy.pdf"
+        >${toNbsp(msg('Privacy Policy'))}</a
+      >
+      •
+      <a
+        target="_blank"
+        href="https://content.rewiringamerica.org/api/terms.pdf"
+        >${toNbsp(msg('Terms', { desc: 'as in terms of service' }))}</a
+      >
+    </p>
+  </div>
+</slot>`;
