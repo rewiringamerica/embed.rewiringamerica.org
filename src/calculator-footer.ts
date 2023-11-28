b5b14349e5e92e72f4a9bba7302e1b276f331896
Tom Carden <tomc@rewiringamerica.org>
2023-11-28 16:58:34 -0700

    adjust footer markup to avoid underlined spaces

--- src/calculator-footer.ts
+++ src/calculator-footer.ts
@@ -6,19 +6,18 @@ const toNbsp = (s: string) => s.replace(' ', '\u00a0');
 export const CALCULATOR_FOOTER = () => html`<div class="calculator__footer">
   <p>
     ${toNbsp(msg('Calculator by', { desc: 'followed by "Rewiring America"' }))}
-    <a target="_blank" href="https://www.rewiringamerica.org">
-      Rewiring&nbsp;America
-    </a>
+    <a target="_blank" href="https://www.rewiringamerica.org"
+      >Rewiring&nbsp;America</a
+    >
     •
     <a
       target="_blank"
       href="https://content.rewiringamerica.org/view/privacy-policy.pdf"
+      >${toNbsp(msg('Privacy Policy'))}</a
     >
-      ${toNbsp(msg('Privacy Policy'))}
-    </a>
     •
-    <a target="_blank" href="https://content.rewiringamerica.org/api/terms.pdf">
-      ${toNbsp(msg('Terms', { desc: 'as in terms of service' }))}
-    </a>
+    <a target="_blank" href="https://content.rewiringamerica.org/api/terms.pdf"
+      >${toNbsp(msg('Terms', { desc: 'as in terms of service' }))}</a
+    >
   </p>
 </div>`;
