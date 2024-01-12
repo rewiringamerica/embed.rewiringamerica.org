c8f8ec6d71ff68fbcbea2a47a51fd67a60f8b2db
Owen Yamauchi <owen@rewiringamerica.org>
2024-01-12 11:07:25 -0500

    Remove state outlines
    ## Description
    
    They're not used anymore.
    
    ## Test Plan
    
    Grep to make sure this is the only place the files were referenced from.

--- src/states.ts
+++ src/states.ts
@@ -2,9 +2,6 @@ import { msg } from '@lit/localize';
 
 export type StateInfo = {
   name: () => string;
-  // The first argument for the URL must be a string literal for the correct
-  // Parcel behavior: https://parceljs.org/languages/javascript/#url-dependencies
-  iconURL: URL;
 };
 
 /**
@@ -13,26 +10,20 @@ export type StateInfo = {
 export const STATES: Record<string, StateInfo> = {
   AZ: {
     name: () => msg('Arizona'),
-    iconURL: new URL('/static/states/AZ.svg', import.meta.url),
   },
   CT: {
     name: () => msg('Connecticut'),
-    iconURL: new URL('/static/states/CT.svg', import.meta.url),
   },
   NY: {
-    name: () => msg(`New York`),
-    iconURL: new URL('/static/states/NY.svg', import.meta.url),
+    name: () => msg('New York'),
   },
   RI: {
     name: () => msg('Rhode Island'),
-    iconURL: new URL('/static/states/RI.svg', import.meta.url),
   },
   VA: {
     name: () => msg('Virginia'),
-    iconURL: new URL('/static/states/VA.svg', import.meta.url),
   },
   VT: {
     name: () => msg('Vermont'),
-    iconURL: new URL('/static/states/VT.svg', import.meta.url),
   },
 };
