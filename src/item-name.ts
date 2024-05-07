c188d8577b4a0f7a979d20ae8e376c494c4d682c
Owen Yamauchi <owen@rewiringamerica.org>
2024-05-07 18:40:05 -0400

    review feedback; add a few more groups and tests

--- src/item-name.ts
+++ src/item-name.ts
@@ -6,7 +6,11 @@ type ItemGroup =
   | 'clothes_dryer'
   | 'generic_heat_pump'
   | 'electric_vehicle'
+  | 'plugin_hybrid'
+  | 'new_vehicle'
+  | 'used_vehicle'
   | 'insulation'
+  | 'weatherization'
   | 'water_heater';
 
 /**
@@ -49,6 +53,21 @@ const ITEM_GROUPS: { group: ItemGroup; members: Set<ItemType> }[] = [
     members: new Set(['new_electric_vehicle', 'used_electric_vehicle']),
   },
   {
+    group: 'plugin_hybrid',
+    members: new Set([
+      'new_plugin_hybrid_vehicle',
+      'used_plugin_hybrid_vehicle',
+    ]),
+  },
+  {
+    group: 'new_vehicle',
+    members: new Set(['new_electric_vehicle', 'new_plugin_hybrid_vehicle']),
+  },
+  {
+    group: 'used_vehicle',
+    members: new Set(['used_electric_vehicle', 'used_plugin_hybrid_vehicle']),
+  },
+  {
     group: 'insulation',
     members: new Set([
       'attic_or_roof_insulation',
@@ -60,166 +79,219 @@ const ITEM_GROUPS: { group: ItemGroup; members: Set<ItemType> }[] = [
     ]),
   },
   {
+    group: 'weatherization',
+    members: new Set([
+      'air_sealing',
+      'attic_or_roof_insulation',
+      'basement_insulation',
+      'crawlspace_insulation',
+      'door_replacement',
+      'duct_replacement',
+      'duct_sealing',
+      'floor_insulation',
+      'wall_insulation',
+      'weatherization',
+      'window_replacement',
+      'efficiency_rebates',
+      'other_insulation',
+      'other_weatherization',
+    ]),
+  },
+  {
     group: 'water_heater',
     members: new Set(['heat_pump_water_heater', 'non_heat_pump_water_heater']),
   },
 ];
 
-/**
- * TODO this is an internationalization sin. Figure out something better!
- */
-export const itemName = (items: ItemType[], msg: MsgFn) => {
-  if (items.length === 1) {
-    const item = items[0];
-    if (item === 'air_sealing') {
-      return msg('air sealing', { desc: 'e.g. "$100 off [this string]"' });
-    } else if (item === 'air_to_water_heat_pump') {
-      return msg('an air-to-water heat pump', {
-        desc: 'e.g. "$100 off [this string]"',
-      });
-    } else if (item === 'attic_or_roof_insulation') {
-      return msg('attic/roof insulation', {
-        desc: 'e.g. "$100 off [this string]"',
-      });
-    } else if (item === 'basement_insulation') {
-      return msg('basement insulation', {
-        desc: 'e.g. "$100 off [this string]"',
-      });
-    } else if (item === 'battery_storage_installation') {
-      return msg('battery storage', { desc: 'e.g. "$100 off [this string]"' });
-    } else if (item === 'central_air_conditioner') {
-      return msg('a central air conditioner', {
-        desc: 'e.g. "$100 off [this string]"',
-      });
-    } else if (item === 'crawlspace_insulation') {
-      return msg('crawlspace insulation', {
-        desc: 'e.g. "$100 off [this string]"',
-      });
-    } else if (item === 'door_replacement') {
-      return msg('door replacement', { desc: 'e.g. "$100 off [this string]"' });
-    } else if (item === 'duct_replacement') {
-      return msg('duct replacement', { desc: 'e.g. "$100 off [this string]"' });
-    } else if (item === 'duct_sealing') {
-      return msg('duct sealing', { desc: 'e.g. "$100 off [this string]"' });
-    } else if (item === 'ducted_heat_pump') {
-      return msg('a ducted heat pump', {
-        desc: 'e.g. "$100 off [this string]"',
-      });
-    } else if (item === 'ductless_heat_pump') {
-      return msg('a ductless heat pump', {
-        desc: 'e.g. "$100 off [this string]"',
-      });
-    } else if (item === 'electric_panel') {
-      return msg('an electric panel', {
-        desc: 'e.g. "$100 off [this string]"',
-      });
-    } else if (item === 'electric_stove') {
-      return msg('an electric/induction stove', {
-        desc: 'e.g. "$100 off [this string]"',
-      });
-    } else if (item === 'electric_vehicle_charger') {
-      return msg('an EV charger', { desc: 'e.g. "$100 off [this string]"' });
-    } else if (item === 'electric_wiring') {
-      return msg('electric wiring', { desc: 'e.g. "$100 off [this string]"' });
-    } else if (item === 'energy_audit') {
-      return msg('an energy audit', { desc: 'e.g. "$100 off [this string]"' });
-    } else if (item === 'floor_insulation') {
-      return msg('floor insulation', { desc: 'e.g. "$100 off [this string]"' });
-    } else if (item === 'geothermal_heating_installation') {
-      return msg('geothermal heating installation', {
-        desc: 'e.g. "$100 off [this string]"',
-      });
-    } else if (item === 'heat_pump_air_conditioner_heater') {
-      return msg('a heat pump', { desc: 'e.g. "$100 off [this string]"' });
-    } else if (item === 'heat_pump_clothes_dryer') {
-      return msg('a heat pump clothes dryer', {
-        desc: 'e.g. "$100 off [this string]"',
-      });
-    } else if (item === 'heat_pump_water_heater') {
-      return msg('a heat pump water heater', {
-        desc: 'e.g. "$100 off [this string]"',
-      });
-    } else if (item === 'new_electric_vehicle') {
-      return msg('a new electric vehicle', {
-        desc: 'e.g. "$100 off [this string]"',
-      });
-    } else if (item === 'new_plugin_hybrid_vehicle') {
-      return msg('a new plug-in hybrid', {
-        desc: 'e.g. "$100 off [this string]"',
-      });
-    } else if (item === 'non_heat_pump_clothes_dryer') {
-      return msg('an electric clothes dryer', {
-        desc: 'e.g. "$100 off [this string]"',
-      });
-    } else if (item === 'non_heat_pump_water_heater') {
-      return msg('an electric water heater', {
-        desc: 'e.g. "$100 off [this string]"',
-      });
-    } else if (item === 'other_heat_pump') {
-      return msg('a heat pump', { desc: 'e.g. "$100 off [this string]"' });
-    } else if (item === 'other_insulation') {
-      return msg('insulation', { desc: 'e.g. "$100 off [this string]"' });
-    } else if (item === 'other_weatherization') {
-      return msg('weatherization', { desc: 'e.g. "$100 off [this string]"' });
-    } else if (item === 'rooftop_solar_installation') {
-      return msg('rooftop solar', { desc: 'e.g. "$100 off [this string]"' });
-    } else if (item === 'used_electric_vehicle') {
-      return msg('a used electric vehicle', {
-        desc: 'e.g. "$100 off [this string]"',
-      });
-    } else if (item === 'used_plugin_hybrid_vehicle') {
-      return msg('a used plug-in hybrid', {
-        desc: 'e.g. "$100 off [this string]"',
-      });
-    } else if (item === 'wall_insulation') {
-      return msg('wall insulation', { desc: 'e.g. "$100 off [this string]"' });
-    } else if (item === 'weatherization') {
-      return msg('weatherization', { desc: 'e.g. "$100 off [this string]"' });
-    } else if (item === 'window_replacement') {
-      return msg('window replacement', {
-        desc: 'e.g. "$100 off [this string]"',
-      });
-    } else if (item === 'efficiency_rebates') {
-      return msg('an energy efficiency retrofit', {
-        desc: 'e.g. "$100 off [this string]"',
-      });
-    }
+const itemsBelongToGroup = (items: ItemType[], members: Set<ItemType>) => {
+  return items.every(i => members.has(i));
+};
 
-    // This will be a type error if the above if-else is not exhaustive
-    const unknownItem: never = item;
-    console.error(`no name for item ${unknownItem}`);
-    return null;
-  }
-
+const multipleItemsName = (items: ItemType[], msg: MsgFn) => {
   // For a multiple-items case, check whether all the items are in one of the
   // defined groups.
   for (const { group, members } of ITEM_GROUPS) {
-    if (items.every(i => members.has(i))) {
-      if (group === 'air_source_heat_pump') {
-        return msg('an air source heat pump', {
-          desc: 'e.g. "$100 off [this string]"',
-        });
-      } else if (group === 'clothes_dryer') {
-        return msg('a clothes dryer', {
-          desc: 'e.g. "$100 off [this string]"',
-        });
-      } else if (group === 'electric_vehicle') {
-        return msg('an electric vehicle', {
-          desc: 'e.g. "$100 off [this string]"',
-        });
-      } else if (group === 'generic_heat_pump') {
-        return msg('a heat pump', { desc: 'e.g. "$100 off [this string]"' });
-      } else if (group === 'insulation') {
-        return msg('insulation', { desc: 'e.g. "$100 off [this string]"' });
-      } else if (group === 'water_heater') {
-        return msg('a water heater', { desc: 'e.g. "$100 off [this string]"' });
-      } else {
-        // This will be a type error if the above if-else is not exhaustive
-        const unknownGroup: never = group;
-        console.error(`no name for ${unknownGroup}`);
+    if (itemsBelongToGroup(items, members)) {
+      switch (group) {
+        case 'air_source_heat_pump':
+          return msg('an air source heat pump', {
+            desc: 'e.g. "$100 off [this string]"',
+          });
+        case 'clothes_dryer':
+          return msg('a clothes dryer', {
+            desc: 'e.g. "$100 off [this string]"',
+          });
+        case 'electric_vehicle':
+          return msg('an electric vehicle', {
+            desc: 'e.g. "$100 off [this string]"',
+          });
+        case 'plugin_hybrid':
+          return msg('a plug-in hybrid', {
+            desc: 'e.g. "$100 off [this string]"',
+          });
+        case 'new_vehicle':
+          return msg('a new vehicle', {
+            desc: 'e.g. "$100 off [this string]"',
+          });
+        case 'used_vehicle':
+          return msg('a used vehicle', {
+            desc: 'e.g. "$100 off [this string]"',
+          });
+        case 'generic_heat_pump':
+          return msg('a heat pump', { desc: 'e.g. "$100 off [this string]"' });
+        case 'insulation':
+          return msg('insulation', { desc: 'e.g. "$100 off [this string]"' });
+        case 'weatherization':
+          return msg('weatherization', {
+            desc: 'e.g. "$100 off [this string]"',
+          });
+        case 'water_heater':
+          return msg('a water heater', {
+            desc: 'e.g. "$100 off [this string]"',
+          });
+        default: {
+          // This will be a type error if the above switch is not exhaustive
+          const unknownGroup: never = group;
+          console.error(`no name for ${unknownGroup}`);
+        }
       }
     }
   }
 
   return null;
+};
+
+/**
+ * TODO this is an internationalization sin. Figure out something better!
+ */
+export const itemName = (items: ItemType[], msg: MsgFn) => {
+  if (items.length > 1) {
+    return multipleItemsName(items, msg);
+  }
+
+  if (items.length !== 1) {
+    return null;
+  }
+
+  const item = items[0];
+  switch (item) {
+    case 'air_sealing':
+      return msg('air sealing', { desc: 'e.g. "$100 off [this string]"' });
+    case 'air_to_water_heat_pump':
+      return msg('an air-to-water heat pump', {
+        desc: 'e.g. "$100 off [this string]"',
+      });
+    case 'attic_or_roof_insulation':
+      return msg('attic/roof insulation', {
+        desc: 'e.g. "$100 off [this string]"',
+      });
+    case 'basement_insulation':
+      return msg('basement insulation', {
+        desc: 'e.g. "$100 off [this string]"',
+      });
+    case 'battery_storage_installation':
+      return msg('battery storage', { desc: 'e.g. "$100 off [this string]"' });
+    case 'central_air_conditioner':
+      return msg('a central air conditioner', {
+        desc: 'e.g. "$100 off [this string]"',
+      });
+    case 'crawlspace_insulation':
+      return msg('crawlspace insulation', {
+        desc: 'e.g. "$100 off [this string]"',
+      });
+    case 'door_replacement':
+      return msg('door replacement', { desc: 'e.g. "$100 off [this string]"' });
+    case 'duct_replacement':
+      return msg('duct replacement', { desc: 'e.g. "$100 off [this string]"' });
+    case 'duct_sealing':
+      return msg('duct sealing', { desc: 'e.g. "$100 off [this string]"' });
+    case 'ducted_heat_pump':
+      return msg('a ducted heat pump', {
+        desc: 'e.g. "$100 off [this string]"',
+      });
+    case 'ductless_heat_pump':
+      return msg('a ductless heat pump', {
+        desc: 'e.g. "$100 off [this string]"',
+      });
+    case 'electric_panel':
+      return msg('an electric panel', {
+        desc: 'e.g. "$100 off [this string]"',
+      });
+    case 'electric_stove':
+      return msg('an electric/induction stove', {
+        desc: 'e.g. "$100 off [this string]"',
+      });
+    case 'electric_vehicle_charger':
+      return msg('an EV charger', { desc: 'e.g. "$100 off [this string]"' });
+    case 'electric_wiring':
+      return msg('electric wiring', { desc: 'e.g. "$100 off [this string]"' });
+    case 'energy_audit':
+      return msg('an energy audit', { desc: 'e.g. "$100 off [this string]"' });
+    case 'floor_insulation':
+      return msg('floor insulation', { desc: 'e.g. "$100 off [this string]"' });
+    case 'geothermal_heating_installation':
+      return msg('geothermal heating installation', {
+        desc: 'e.g. "$100 off [this string]"',
+      });
+    case 'heat_pump_air_conditioner_heater':
+      return msg('a heat pump', { desc: 'e.g. "$100 off [this string]"' });
+    case 'heat_pump_clothes_dryer':
+      return msg('a heat pump clothes dryer', {
+        desc: 'e.g. "$100 off [this string]"',
+      });
+    case 'heat_pump_water_heater':
+      return msg('a heat pump water heater', {
+        desc: 'e.g. "$100 off [this string]"',
+      });
+    case 'new_electric_vehicle':
+      return msg('a new electric vehicle', {
+        desc: 'e.g. "$100 off [this string]"',
+      });
+    case 'new_plugin_hybrid_vehicle':
+      return msg('a new plug-in hybrid', {
+        desc: 'e.g. "$100 off [this string]"',
+      });
+    case 'non_heat_pump_clothes_dryer':
+      return msg('an electric clothes dryer', {
+        desc: 'e.g. "$100 off [this string]"',
+      });
+    case 'non_heat_pump_water_heater':
+      return msg('an electric water heater', {
+        desc: 'e.g. "$100 off [this string]"',
+      });
+    case 'other_heat_pump':
+      return msg('a heat pump', { desc: 'e.g. "$100 off [this string]"' });
+    case 'other_insulation':
+      return msg('insulation', { desc: 'e.g. "$100 off [this string]"' });
+    case 'other_weatherization':
+      return msg('weatherization', { desc: 'e.g. "$100 off [this string]"' });
+    case 'rooftop_solar_installation':
+      return msg('rooftop solar', { desc: 'e.g. "$100 off [this string]"' });
+    case 'used_electric_vehicle':
+      return msg('a used electric vehicle', {
+        desc: 'e.g. "$100 off [this string]"',
+      });
+    case 'used_plugin_hybrid_vehicle':
+      return msg('a used plug-in hybrid', {
+        desc: 'e.g. "$100 off [this string]"',
+      });
+    case 'wall_insulation':
+      return msg('wall insulation', { desc: 'e.g. "$100 off [this string]"' });
+    case 'weatherization':
+      return msg('weatherization', { desc: 'e.g. "$100 off [this string]"' });
+    case 'window_replacement':
+      return msg('window replacement', {
+        desc: 'e.g. "$100 off [this string]"',
+      });
+    case 'efficiency_rebates':
+      return msg('an energy efficiency retrofit', {
+        desc: 'e.g. "$100 off [this string]"',
+      });
+    default: {
+      // This will be a type error if the above if-else is not exhaustive
+      const unknownItem: never = item;
+      console.error(`no name for item ${unknownItem}`);
+      return null;
+    }
+  }
 };
--- test/item-names.test.ts
+++ test/item-names.test.ts
@@ -11,4 +11,50 @@ describe('group names', () => {
       itemName(['ducted_heat_pump', 'geothermal_heating_installation'], msg),
     ).toBe('a heat pump');
   });
+
+  test('weatherization and insulation', () => {
+    expect(
+      itemName(['attic_or_roof_insulation', 'basement_insulation'], msg),
+    ).toBe('insulation');
+    expect(itemName(['attic_or_roof_insulation', 'air_sealing'], msg)).toBe(
+      'weatherization',
+    );
+    expect(itemName(['wall_insulation', 'other_insulation'], msg)).toBe(
+      'insulation',
+    );
+    expect(itemName(['wall_insulation', 'other_weatherization'], msg)).toBe(
+      'weatherization',
+    );
+    expect(itemName(['other_weatherization', 'energy_audit'], msg)).toBeNull();
+  });
+
+  test('vehicles', () => {
+    expect(
+      itemName(['new_electric_vehicle', 'used_electric_vehicle'], msg),
+    ).toBe('an electric vehicle');
+    expect(
+      itemName(
+        ['new_plugin_hybrid_vehicle', 'used_plugin_hybrid_vehicle'],
+        msg,
+      ),
+    ).toBe('a plug-in hybrid');
+    expect(
+      itemName(['new_electric_vehicle', 'new_plugin_hybrid_vehicle'], msg),
+    ).toBe('a new vehicle');
+    expect(
+      itemName(['used_electric_vehicle', 'used_plugin_hybrid_vehicle'], msg),
+    ).toBe('a used vehicle');
+
+    expect(
+      itemName(
+        [
+          'new_electric_vehicle',
+          'used_electric_vehicle',
+          'new_plugin_hybrid_vehicle',
+          'used_plugin_hybrid_vehicle',
+        ],
+        msg,
+      ),
+    ).toBeNull();
+  });
 });
