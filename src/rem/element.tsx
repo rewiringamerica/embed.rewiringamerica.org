e60b8db62d4dd75c5f7f70dce0077eb1057f03b3
Owen Yamauchi <owen@rewiringamerica.org>
2025-06-09 19:42:14 -0400

    build form, no api call yet

--- src/rem/element.tsx
+++ src/rem/element.tsx
@@ -1,21 +1,68 @@
 import tailwindStyles from 'bundle-text:../tailwind.css';
-import { FC } from 'react';
-import { createRoot, Root } from 'react-dom/client';
+import { FC, useState } from 'react';
+import { Root, createRoot } from 'react-dom/client';
 import { DEFAULT_CALCULATOR_API_HOST } from '../api/fetch';
 import { CalculatorFooter } from '../calculator-footer';
+import { TextButton } from '../components/buttons';
+import { Card } from '../components/card';
 import { allLocales } from '../i18n/locales';
-import { LocaleContext } from '../i18n/use-translated';
+import { LocaleContext, useTranslated } from '../i18n/use-translated';
+import { safeLocalStorage } from '../safe-local-storage';
+import { FormValues, RemForm } from './form';
+
+/**
+ * If you make a backward-incompatible change to the format of form value
+ * storage, increment the version in this key.
+ */
+const FORM_VALUES_LOCAL_STORAGE_KEY = 'RA-calc-rem-form-values-v1';
+declare module '../safe-local-storage' {
+  interface SafeLocalStorageMap {
+    [FORM_VALUES_LOCAL_STORAGE_KEY]: Partial<FormValues>;
+  }
+}
 
 const RemCalculator: FC<{
   shadowRoot: ShadowRoot;
   apiHost: string;
   apiKey: string;
 }> = ({ apiHost, apiKey }) => {
+  const { msg } = useTranslated();
+
+  const [formKey, setFormKey] = useState(0);
+
+  // Incrementing the form key will cause the form to drop its state,
+  // reinitializing the input values from initial (constructed above).
+  const resetFormValues = () => {
+    safeLocalStorage.removeItem(FORM_VALUES_LOCAL_STORAGE_KEY);
+    setFormKey(fk => fk + 1);
+  };
+
+  const submitForm = (formValues: FormValues) => {
+    safeLocalStorage.setItem(FORM_VALUES_LOCAL_STORAGE_KEY, formValues);
+    // set snapshot
+    console.log(formValues);
+  };
+
   return (
     <div>
-      {apiHost}
-      <br />
-      {apiKey}
+      <Card padding="medium" theme="grey" isFlat>
+        <div className="flex justify-between items-baseline">
+          <h1 className="text-base sm:text-md font-medium leading-tight">
+            {msg('Your household info')}
+          </h1>
+          <div>
+            <TextButton onClick={resetFormValues}>{msg('Reset')}</TextButton>
+          </div>
+        </div>
+
+        <div className="text-sm leading-normal">
+          {msg(
+            'Enter your household information to calculate the energy bill savings and emissions reductions you could get from upgrades to your home.',
+          )}
+        </div>
+
+        <RemForm key={formKey} loading={false} onSubmit={submitForm} />
+      </Card>
     </div>
   );
 };
