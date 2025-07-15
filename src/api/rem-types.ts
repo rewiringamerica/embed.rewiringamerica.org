8c044bee26b0aff7c53324c41f05a59a353cc0b4
Owen Yamauchi <owen@rewiringamerica.org>
2025-07-15 17:24:20 -0400

    Add internal-only upgrades; allow customization
    ## Links
    
    - [Jira](https://rewiringamerica.atlassian.net/browse/RAT-654)
    
    ## Description
    
    These two things are related:
    
    - Give the calculator widget an attribute that can contain a
      comma-separated list of upgrades. The options are the exact strings
      that get passed to the API. These are kinda ugly and not
      user-friendly, but we're expecting only trusted partners to use this
      feature, so it's not a concern.
    
      - If there's an invalid upgrade in the attribute, the whole widget
        fails to render.
    
      - The options will show up in the UI in the exact order given, with
        the exception that the water heater option will not show up at all
        if the user doesn't select a water heating fuel.
    
      - If the attribute isn't populated, the same options as before will
        be shown (in the same order).
    
    - Add the internal-only upgrade options that the REM API supports. If
      one of these is selected, use the internal REM endpoint instead of
      the public one. (Otherwise, continue to use the public endpoint so
      the embedder doesn't need an API key with private access.)
    
    ## Test Plan
    
    1. Look at the widget without the attribute; make sure it displays and
       behaves as before. Compare selecting a water heating fuel vs. not.
    
    2. Add the attribute with nonsense contents; make sure the widget
       fails to render.
    
    3. Add the attribute `hvac__internal_only_heat_pump_ductless_carrier_performance,hvac__heat_pump_seer18_hspf10`;
       make sure it shows the RI package followed by the regular heat pump
       package.
    
       1. With a non-private-access API key, click the regular package and
          make sure it loads results. Click the RI package and make sure
          there's a sensible error.
    
       2. With a private-access API key, click both packages and make sure
          they both load results.

--- src/api/rem-types.ts
+++ src/api/rem-types.ts
@@ -3,7 +3,20 @@ export enum Upgrade {
   HeatPumpAndWeatherization = 'combination__hvac_seer18_hspf10__weatherization',
   WaterHeater = 'water_heater__heat_pump_uef3.35',
   Weatherization = 'weatherization__insulation_air_duct_sealing',
+
+  // The options below are not shown by default, and require the use of the
+  // private API endpoint.
+  InternalDaikin7 = 'hvac__internal_only_heat_pump_ducted_daikin_7',
+  InternalCarrierPerformance = 'hvac__internal_only_heat_pump_ductless_carrier_performance',
+  InternalDaikinSMulti = 'hvac__internal_only_heat_pump_ductless_daikin_smulti',
 }
+
+/** Upgrades that require using the internal API endpoint. */
+export const INTERNAL_UPGRADES = new Set([
+  Upgrade.InternalCarrierPerformance,
+  Upgrade.InternalDaikin7,
+  Upgrade.InternalDaikinSMulti,
+]);
 
 export enum HeatingFuel {
   Electricity = 'electricity',
--- src/rem/UpgradeOptions.tsx
+++ src/rem/UpgradeOptions.tsx
@@ -4,56 +4,76 @@ import { Card } from '../components/card';
 import { MsgFn } from '../i18n/msg';
 import { useTranslated } from '../i18n/use-translated';
 
-function getOptions(includeWaterHeater: boolean, msg: MsgFn) {
-  const result = [
-    {
-      upgrade: Upgrade.HeatPump,
-      label: msg('Heat pump'),
-      description:
-        msg(`A heat pump is an efficient electric system that provides both \
+const LABELS: { [k in Upgrade]: (m: MsgFn) => string } = {
+  [Upgrade.HeatPump]: msg => msg('Heat pump'),
+  [Upgrade.HeatPumpAndWeatherization]: msg => msg('Heat pump + weatherization'),
+  [Upgrade.Weatherization]: msg => msg('Weatherization'),
+  [Upgrade.WaterHeater]: msg => msg('Heat pump water heater'),
+
+  [Upgrade.InternalCarrierPerformance]: msg => msg('Heat pump (RI package)'),
+  [Upgrade.InternalDaikin7]: msg => msg('Heat pump (CO ducted package)'),
+  [Upgrade.InternalDaikinSMulti]: msg => msg('Heat pump (CO ductless package)'),
+};
+
+const DESCRIPTIONS: { [k in Upgrade]: (m: MsgFn) => string } = {
+  [Upgrade.HeatPump]: msg =>
+    msg(`A heat pump is an efficient electric system that provides both \
 heating and cooling by transferring heat between your home and the air \
 outside, cutting energy use compared to traditional systems.`),
-    },
-    {
-      upgrade: Upgrade.Weatherization,
-      label: msg('Weatherization'),
-      description:
-        msg(`Weatherization includes upgrades like sealing air leaks and adding \
-insulation to reduce energy loss, improve comfort, and lower heating and \
-cooling bills.`),
-    },
-    {
-      upgrade: Upgrade.HeatPumpAndWeatherization,
-      label: msg('Heat pump + weatherization'),
-      description:
-        msg(`Combining a heat pump with weatherization (like insulation and air \
+  [Upgrade.HeatPumpAndWeatherization]: msg =>
+    msg(`Combining a heat pump with weatherization (like insulation and air \
 sealing) maximizes comfort and energy savings by improving your home’s \
 ability to retain heated or cooled air.`),
-    },
-  ];
-
-  if (includeWaterHeater) {
-    result.push({
-      upgrade: Upgrade.WaterHeater,
-      label: msg('Heat pump water heater'),
-      description:
-        msg(`A heat pump water heater uses electricity to pull heat from the \
+  [Upgrade.Weatherization]: msg =>
+    msg(`Weatherization includes upgrades like sealing air leaks and adding \
+insulation to reduce energy loss, improve comfort, and lower heating and \
+cooling bills.`),
+  [Upgrade.WaterHeater]: msg =>
+    msg(`A heat pump water heater uses electricity to pull heat from the \
 surrounding air to warm your water, using up to 70% less energy than \
 conventional electric water heaters.`),
-    });
+
+  // There are actually two standard packages in RI (Carrier Performance and
+  // Carrier Infinity) but only the former is supported in the REM API.
+  [Upgrade.InternalCarrierPerformance]: msg =>
+    msg(`This is the ductless heat pump package negotiated with our preferred \
+contractor(s) in Rhode Island. (Carrier Performance series)`),
+  [Upgrade.InternalDaikin7]: msg =>
+    msg(`This is the ducted heat pump package negotiated with our preferred \
+contractor(s) in Colorado.`),
+  [Upgrade.InternalDaikinSMulti]: msg =>
+    msg(`This is the ductless heat pump package negotiated with our preferred \
+contractor(s) in Colorado.`),
+};
+
+function getOptions(
+  upgrades: Upgrade[],
+  includeWaterHeater: boolean,
+  msg: MsgFn,
+) {
+  const result = upgrades.map(upgrade => ({
+    upgrade,
+    label: LABELS[upgrade](msg),
+    description: DESCRIPTIONS[upgrade](msg),
+  }));
+
+  if (!includeWaterHeater) {
+    return result.filter(({ upgrade }) => upgrade !== Upgrade.WaterHeater);
+  } else {
+    return result;
   }
-  return result;
 }
 
 export const UpgradeOptions: FC<{
+  upgrades: Upgrade[];
   includeWaterHeater: boolean;
   onUpgradeSelected: (u: Upgrade, l: string) => void;
-}> = ({ includeWaterHeater, onUpgradeSelected }) => {
+}> = ({ upgrades, includeWaterHeater, onUpgradeSelected }) => {
   const { msg } = useTranslated();
 
   const cards = [];
 
-  for (const option of getOptions(includeWaterHeater, msg)) {
+  for (const option of getOptions(upgrades, includeWaterHeater, msg)) {
     cards.push(
       <Card theme="white" padding="small" key={option.upgrade} isFlat>
         <div className="flex flex-col gap-3">
